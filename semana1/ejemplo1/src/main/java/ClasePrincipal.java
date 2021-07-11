d0d6fec129b3740f1e11abed144ccc2990660c1b
Henry Arcila <helpu1409@gmail.com>
2021-07-10 21:37:01 -0500

    Agregando clase principal

---
+++ semana1/ejemplo1/src/main/java/ClasePrincipal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClasePrincipal {
+    public static void main (String[] args) {          
+        System.out.println("¡Hola, Mundo!");
+    }
+}  
